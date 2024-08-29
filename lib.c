937f12bdcb2d989c14e8a052356d5da58ea24cff
Ahmed Bouzaffour <ahmedbouzaffour2002@gmail.com>
2024-08-29 10:32:05 -0600

    Handelling inputs

--- lib.c
+++ lib.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 
+#define UNEMPLEMENTED \
+	do {	\
+		fprintf(stderr,"%s:%d: TODO: %s is not implemented yet.\n",\
+						__FILE__,__LINE__,__func__);\
+		abort();\
+		}while(0)
 
+
+typedef struct
+{
+	char* buffer;
+	size_t buffer_length;
+	size_t input_length;
+}
+
+
+void read_input(InputBuffer* input_buffer) {
+	ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+	if (bytes_read <= 0) {
+		printf("Error reading input\n");
+		exit(EXIT_FAILURE);
+	}
+     
+	input_buffer->input_length = bytes_read - 1;
+	input_buffer->buffer[bytes_read - 1] = 0;
+}
+
+void close_input_buffer(InputBuffer* input_buffer) {
+	free(input_buffer->buffer);
+	free(input_buffer);
+}
 void input_prompt(){printf("lib >");}
 
 int main()
