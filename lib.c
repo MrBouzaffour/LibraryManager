44bc2ef237435ae38e1a6e4ed7cfac8953c927bc
Ahmed Bouzaffour <ahmedbouzaffour2002@gmail.com>
2024-08-29 11:22:26 -0600

    fixed a bug in meta commands

--- lib.c
+++ lib.c
@@ -77,6 +77,14 @@ int main()
 	{
 		input_prompt();
 		read_input(input_buffer);
-
+		 if (input_buffer->buffer[0] == '.'){
+			 switch( do_meta_command(input_buffer)){
+				 case(META_COMMAND_SUCCESS):
+					 continue;
+				 case(META_COMMAND_UNRECOGNIZED):
+					 printf("Unrecognized command '%s'\n", input_buffer->buffer);
+					 continue;
+			 }
+		 }
 	}
 }
