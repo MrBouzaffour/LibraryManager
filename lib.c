aca34a3049a90a3ce6eb6f1a2046a5d4468eff7b
Ahmed Bouzaffour <ahmedbouzaffour2002@gmail.com>
2024-08-29 10:41:35 -0600

    init empty inputBuffer

--- lib.c
+++ lib.c
@@ -16,6 +16,15 @@ typedef struct
 }
 
 
+InputBuffer* new_input_buffer() {
+	
+	nputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
+	input_buffer->buffer = NULL;
+        input_buffer->buffer_length = 0;
+	input_buffer->input_length = 0;
+	return input_buffer;
+	}
+
 void read_input(InputBuffer* input_buffer) {
 	ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
 	if (bytes_read <= 0) {
